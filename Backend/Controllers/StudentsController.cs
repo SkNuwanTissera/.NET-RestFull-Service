60243ac994a09c7dabf418ecdcf0a8f512b7ac02
it15120212 <it15120212@402-56>
2018-07-29 12:47:23 +0530

    Add project files.

---
+++ Backend/Controllers/StudentsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Backend.Models;
+
+namespace Backend.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        Student[] students = new Student[]
+            {
+                new Student { id =1 , GuardianName="Nuwan Tissera", Name="Rasika Tissera", School="Visaka" },
+                new Student { id =2 , GuardianName="Hello Tissera", Name="Hello Tissera", School="Visaka" },
+                new Student { id =3 , GuardianName="Prasanna Tissera", Name="Nuwan Tissera", School="Ananda" },
+                new Student { id =4 , GuardianName="Nuwan Tissera", Name="Prasanna Tissera", School="Ananda" }
+            };
+
+        [HttpGet]
+        public IEnumerable<Student> GetAllStudents() {
+            return students;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetStudent(int id)
+        {
+            var student = students.FirstOrDefault((s) => s.id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public void myPostmethod() { System.Diagnostics.Debug.WriteLine("My Post Method is called"); }
+    }
+}
---
+++ Backend/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public String Name { get; set; }
+        public String School { get; set; }
+        public String Grade { get; set; }
+        public String GuardianName { get; set; }
+    }
+}
